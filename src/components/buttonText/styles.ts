de8ba2fcc1273464e8ecfccf0f445e43196c4893
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-19 18:14:36 -0300

    Add padding, border-radius, and hover effect to button text styles

--- src/components/buttonText/styles.ts
+++ src/components/buttonText/styles.ts
@@ -6,4 +6,12 @@ export const Container = styled.button`
   cursor: pointer;
   color: ${({ theme }) => theme.COLORS.GREEN};
   font-size: 1rem;
+  padding: 0.4rem;
+  border-radius: 0.25rem;
+
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.COLORS['BG-700']};
+  }
 `
