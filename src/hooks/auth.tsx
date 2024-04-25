8a75e373c0204bce3ec3ec233b3a9b3a31d2461c
Kayo <Kayohenriquesilva19@gmail.com>
2024-04-25 07:53:46 -0300

    Refactor authContext hook to include useEffect for token and user persistence

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext, useContext, ReactNode, useState } from 'react'
+import {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+  useEffect,
+} from 'react'
 import { api } from '../services/api'
 
 interface User {
@@ -26,14 +32,21 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User | null>(null)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [token, setToken] = useState<string | null>(null)
 
   async function signIn({ email, password }: SignInCredentials): Promise<void> {
     try {
       const response = await api.post('/sessions', { email, password })
       const { user, token } = response.data
+      console.log(response.data)
+
+      localStorage.setItem('@rocketnotes:token', token)
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
 
       api.defaults.headers.authorization = `Bearer ${token}`
       setUser(user)
+      setToken(token)
     } catch (error) {
       if ((error as any).response) {
         alert((error as any).response.data.message)
@@ -42,6 +55,17 @@ function AuthProvider({ children }: AuthProviderProps) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@rocketnotes:token')
+    const user = localStorage.getItem('@rocketnotes:user')
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+      setUser(JSON.parse(user))
+      setToken(token)
+    }
+  }, [])
 
   return (
     <AuthContext.Provider value={{ user, signIn }}>
